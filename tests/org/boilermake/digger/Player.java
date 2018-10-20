669353f8397fe0a2164a33a3468cdc51e41ebaf1
Jordan Buckmaster <buckmast@purdue.edu>
2018-10-20 17:52:49 -0400

    Work on player collisions

--- tests/org/boilermake/digger/Player.java
+++ tests/org/boilermake/digger/Player.java
@@ -139,6 +139,14 @@ public class Player extends Block {
 		this.velocity.y += 2*acceleration.y;
 	}
 
+	public void stopX() {
+		this.velocity.x = 0;
+	}
+
+	public void stopY() {
+		this.velocity.y = 0;
+	}
+
 	public HitBox useItem() {
 	    switch(this.item) {
 			default: break;
