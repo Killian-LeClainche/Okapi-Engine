a7d464c1b35f3e6e67dc9905508f967957115181
Killian-LeClainche <kleclain@purdue.edu>
2018-10-20 14:26:02 -0400

    Add texture shaders

---
+++ resources/shaders/pos_color_texture.vert
@@ -0,0 +1,15 @@
+#version 330 core
+
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec4 color;
+layout(location = 3) in vec2 texCoord;
+
+out vec4 in_Color;
+out vec2 in_TexCoord;
+
+void main()
+{
+    in_Color = color;
+    in_TexCoord = texCoord;
+    gl_Position = vec4(position.x / 860 - 1, position.y / 540 - 1, position.z, 1.0);
+}
--- tests/org/boilermake/digger/DiggerRenderer.kt
+++ tests/org/boilermake/digger/DiggerRenderer.kt
@@ -3,6 +3,7 @@ package org.boilermake.digger
 import org.lwjgl.opengl.GL20C
 import org.lwjgl.opengl.GL33C
 import polaris.okapi.render.DrawArray
+import polaris.okapi.render.Texture
 import polaris.okapi.render.VertexAttributes
 
 /**
@@ -16,6 +17,8 @@ class DiggerRenderer(private val world: DiggerWorld) {
 
     var timeExisted: Double = 0.0
 
+    var shaderTexID: Int = 0
+
     fun init() {
 
         val pos = world.playerList[0].position;
@@ -23,24 +26,31 @@ class DiggerRenderer(private val world: DiggerWorld) {
         val quadArray = floatArrayOf(
                 //position          color                   texture
                 //x,y,z             r,g,b,a                 u,v
-                pos.x - 30f, pos.y - 30f, 0f,       1f, 0f, 0f, 1f,
-                pos.x + 30f, pos.y - 30f, 0f,       1f, 0f, 0f, 1f,
-                pos.x + 30f, pos.y + 30f, 0f,       1f, 0f, 0f, 1f,
+                pos.x - 60f, pos.y - 60f, 0f,       1f, 1f, 1f, 1f,     0f, 1f,
+                pos.x + 60f, pos.y - 60f, 0f,       1f, 1f, 1f, 1f,     1f, 1f,
+                pos.x + 60f, pos.y + 60f, 0f,       1f, 1f, 1f, 1f,     1f, 0f,
 
-                pos.x + 30f, pos.y + 30f, 0f,       1f, 0f, 0f, 1f,
-                pos.x - 30f, pos.y - 30f, 0f,       1f, 0f, 0f, 1f,
-                pos.x - 30f, pos.y + 30f, 0f,       1f, 0f, 0f, 1f
+                pos.x + 60f, pos.y + 60f, 0f,       1f, 1f, 1f, 1f,     1f, 0f,
+                pos.x - 60f, pos.y - 60f, 0f,       1f, 1f, 1f, 1f,     0f, 1f,
+                pos.x - 60f, pos.y + 60f, 0f,       1f, 1f, 1f, 1f,     0f, 1f
         )
 
-        quad = DrawArray(GL20C.GL_TRIANGLES, GL20C.GL_DYNAMIC_DRAW, quadArray, 6, VertexAttributes.POS_COLOR)
+        quad = DrawArray(GL20C.GL_TRIANGLES, GL20C.GL_DYNAMIC_DRAW, quadArray, 6, VertexAttributes.POS_COLOR_TEXTURE)
+
+        world["player"] = "resources/digger/character.png"
     }
 
     fun render(delta: Double) {
+
+        val texture: Texture = world["player"]
+
         timeExisted += delta
         world.application.updateView()
-        world.application.renderManager.posColorShader.bind()
+        world.application.renderManager.posColorTextureShader.bind()
 
-        VertexAttributes.POS_COLOR.enable()
+        VertexAttributes.POS_COLOR_TEXTURE.enable()
+
+        Texture.enable()
 
         quad.bind()
 
@@ -49,13 +59,13 @@ class DiggerRenderer(private val world: DiggerWorld) {
         quad.array = floatArrayOf(
                 //position          color                   texture
                 //x,y,z             r,g,b,a                 u,v
-                pos.x - 30f, pos.y - 30f, 0f,       1f, 0f, 0f, 1f,
-                pos.x + 30f, pos.y - 30f, 0f,       1f, 0f, 0f, 1f,
-                pos.x + 30f, pos.y + 30f, 0f,       1f, 0f, 0f, 1f,
+                pos.x - 60f, pos.y - 60f, 0f,       1f, 1f, 1f, 1f,     0f, 1f,
+                pos.x + 60f, pos.y - 60f, 0f,       1f, 1f, 1f, 1f,     1f, 1f,
+                pos.x + 60f, pos.y + 60f, 0f,       1f, 1f, 1f, 1f,     1f, 0f,
 
-                pos.x + 30f, pos.y + 30f, 0f,       1f, 0f, 0f, 1f,
-                pos.x - 30f, pos.y - 30f, 0f,       1f, 0f, 0f, 1f,
-                pos.x - 30f, pos.y + 30f, 0f,       1f, 0f, 0f, 1f
+                pos.x + 60f, pos.y + 60f, 0f,       1f, 1f, 1f, 1f,     1f, 0f,
+                pos.x - 60f, pos.y - 60f, 0f,       1f, 1f, 1f, 1f,     0f, 1f,
+                pos.x - 60f, pos.y + 60f, 0f,       1f, 1f, 1f, 1f,     0f, 0f
         )
 
         quad.draw()
