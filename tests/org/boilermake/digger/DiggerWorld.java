6522af0d90ae6d2b6f98a1fc9ff4c6377c2f6a4e
NQureshi850 <nameer.qureshi@gmail.com>
2018-10-21 08:43:13 -0400

    map fixes

--- tests/org/boilermake/digger/DiggerWorld.java
+++ tests/org/boilermake/digger/DiggerWorld.java
@@ -90,7 +90,7 @@ public class DiggerWorld extends World {
 		mapList.add(map4);
 		mapList.add(map5);
 		
-		int randomVal = 1;
+		int randomVal = random(0,4);
 		
 		terrainList.addAll(mapList.get(randomVal).terrainList);
 		graveList.addAll(mapList.get(randomVal).graveList);
